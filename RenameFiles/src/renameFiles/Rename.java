113c2826bad8b03f5d386f858c8506a9a7c67a4b
Jason <Jason@DESKTOP-PS0193D.home>
2020-04-29 14:58:39 +0300

    Methods cleared

--- RenameFiles/src/renameFiles/Rename.java
+++ RenameFiles/src/renameFiles/Rename.java
@@ -11,39 +11,43 @@ import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 
 public class Rename {
+	private String fileType = ".docx";
 	File folder = new File(".");				//get the current path
 	File[] files = folder.listFiles();			//get a list of all the files
 	
 	public void renameFiles() {
 		for (File file : files) {
 			String fileName = file.getName();	//get the name of every file
-			if(fileName.endsWith(".docx") || fileName.endsWith(".doc")) {	//if the file is a .docx
-				Path filePath = file.toPath();		//get the path of the file
-				BasicFileAttributes attr;
-				try {
-					attr = Files.readAttributes(filePath, BasicFileAttributes.class);	//create the attributes
-					FileTime date = attr.creationTime();								//get the creation time
-					SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");			//get a date format we want
-					String dateCreated = df.format(date.toMillis());					//create a string with the date
-					
-					File fileNew = new File(dateCreated + "_" + fileName);				//create the new file name
-					
-					
-					boolean success = file.renameTo(fileNew);							//rename the file
-					
-					if(success) {
-						System.out.println("Files renamed successfully");
-					}
-					else {
-						System.out.println("PROBLEM");
-					}
-				} catch (IOException e) {
-					JOptionPane.showMessageDialog(null, "There are no .docx files");
-					e.printStackTrace();
-				}
-			}
+			changeFileName(file, fileName, fileType);
 		}
 	}
 	
+	public void changeFileName(File file, String fileName, String fileType) {
+		if(fileName.endsWith(fileType)) {	//if the file is a .docx
+			Path filePath = file.toPath();		//get the path of the file
+			BasicFileAttributes attr;
+			try {
+				attr = Files.readAttributes(filePath, BasicFileAttributes.class);	//create the attributes
+				FileTime date = attr.creationTime();								//get the creation time
+				SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");			//get a date format we want
+				String dateCreated = df.format(date.toMillis());					//create a string with the date
+				
+				File fileNew = new File(dateCreated + "_" + fileName);				//create the new file name
+				
+				
+				boolean success = file.renameTo(fileNew);							//rename the file
+				
+				if(success) {
+					System.out.println("Files renamed successfully");
+				}
+				else {
+					System.out.println("PROBLEM");
+				}
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "There are no .docx files");
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
